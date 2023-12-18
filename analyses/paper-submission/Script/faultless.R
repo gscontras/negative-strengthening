e664e55b26608ea1d23cf8c24395910d69fb2f78
Greg Scontras <gscontras@gmail.com>
2023-12-18 10:29:58 -0800

    initial faultless results

---
+++ analyses/paper-submission/Script/faultless.R
@@ -0,0 +1,207 @@
+
+########################################################################
+#### Analysis of the role of subjectivity in negative strengthening ####
+###################### 18.12.2023 ######################################
+########################################################################
+
+####################################
+#### load new faultless data ####
+####################################
+
+df = read.csv("../data/results_faultless.csv",header=T)
+
+unique(df$language)
+
+length(unique(df$participant_id)) # n=40
+
+## remove non-English speakers
+d = df[df$language!=""&
+         df$language!="GREG"
+       ,]
+unique(d$language)
+length(unique(d$participant_id)) # n=38
+
+## load helper file for bootstrapped CIs
+source("helpers.r")
+
+colnames(d)[4] <- "full_predicate"
+colnames(d)[5] <- "predicate"
+colnames(d)[6] <- "valence"
+
+d$negated="false"
+
+head(d)
+
+agr = bootsSummary(data=d, measurevar="response", groupvars=c("predicate","valence","negated"))
+
+mean(agr$N) ## 21.11
+
+agr_mean = bootsSummary(data=d, measurevar="response", groupvars=c("valence","negated"))
+agr_mean
+
+
+#################################################
+#### load negative strengthening data  ##########
+#################################################
+
+crit<-read.delim("../data/facethreat_anonymous.txt",header=T, dec=",")
+crit2<-read.delim("../data/facethreat_standard-nonstandard_anonymous.txt",header=T, dec=",")
+crit$person<-NULL
+crit$context<-NULL
+crit$adjective<-NULL
+crit2$person<-NULL
+crit2$context<-NULL
+crit2$adjective<-NULL
+crit$exp = "e1"
+crit2$exp = "e2"
+
+crit<-rbind(crit, crit2)
+crit<-crit[crit$Gender!="",]
+str(crit)
+head(crit)
+
+e<-crit
+head(e)
+summary(e)
+str(e)
+
+e$adjective = NA
+
+e$item1 <- as.character(e$item1)
+e$item2 <- as.character(e$item2)
+e[e$polarity=="Pos",]$adjective <- e[e$polarity=="Pos",]$item1
+e[e$polarity=="Neg",]$adjective <- e[e$polarity=="Neg",]$item2
+e$notAdj_subj = agr$response[match(paste("not",tolower(e$adjective)),agr$predicate)]
+e$adj_subj = agr$response[match(tolower(e$adjective),agr$predicate)]
+
+e$ant = NA
+e[e$polarity=="Pos",]$ant <- e[e$polarity=="Pos",]$item2
+e[e$polarity=="Neg",]$ant <- e[e$polarity=="Neg",]$item1
+e$ant_subj = agr$response[match(tolower(e$ant),agr$predicate)]
+
+e$diff=NA
+e$diff = e$notAdj_subj - e$ant_subj
+
+head(e)
+
+###contrast coding
+library(ordinal)
+e$valuef<-factor(e$value)
+e$polarity<-factor(e$polarity)
+e$polarity<-relevel(e$polarity,ref="Pos")
+contrasts(e$polarity) <- "contr.sum"
+
+head(e)
+length(unique(e$item))
+
+##centering
+e$cdiff<-e$diff-mean(e$diff)
+e$cadj_subj<-e$adj_subj-mean(e$adj_subj)
+
+
+#########################
+###### mixed model ######
+#########################
+
+###### analyzing subjectivity directly
+
+### maximal model subjectivity
+mfulladj<-clmm(valuef~polarity*cadj_subj+ (1+polarity|item) + (1+polarity|Worker_ID) , data =e)
+summary(mfulladj)
+
+## without "accurate"
+
+e_new = e[e$item!="Accurate_Inaccurate",]
+
+length(unique(e_new$item))
+
+newfull<-clmm(valuef~polarity*cadj_subj+ (1+polarity|item) + (1+polarity|Worker_ID) , data =e_new)
+summary(newfull)
+
+
+##################
+###### plot ######
+##################
+
+library(ggplot2)
+library(Rmisc)
+library(ggrepel)
+
+
+ag <- summarySE(e, measurevar="value", groupvars=c("polarity","adjective","adj_subj","item"),na.rm=T)
+
+ag$polarity<-factor(ag$polarity,levels=c("Pos","Neg"))
+ggplot(ag, aes(y=value,x=adj_subj, colour=polarity, label=adjective)) +
+  labs(y='negative strengthening\n', x="\nfaultless disagreement",colour="adjective\npolarity") +
+  scale_color_manual(labels = c("positive", "negative"),values=c("blue","red"))+
+  geom_text_repel()+
+  #geom_text()+
+  geom_smooth(method="lm")+
+  geom_point(size=2)+ 
+  #xlim(0.25,0.85)+
+  theme_bw() #+
+  #theme(text = element_text(size=14))
+#ggsave("../Plots/adjective-faultless.pdf",width=8 ,height=6)
+
+
+
+#######################################
+###### bootstrapping correlation ######
+#######################################
+
+library(hydroGOF)
+library(boot)
+
+
+###simple adj
+###pos
+p = aggregate(value~polarity*adjective*adj_subj*item,data=e[e$polarity=="Pos",],FUN=mean)
+gof(p$value,p$adj_subj)
+results <- boot(data=p, statistic=rsq, R=10000, formula=value~adj_subj)
+boot.ci(results, type="bca") 
+#r           0.62
+#R2          0.39
+#95%   ( 0.0588,  0.7486 )  
+
+###neg
+n = aggregate(value~polarity*adjective*adj_subj*item,data=e[e$polarity=="Neg",],FUN=mean)
+gof(n$value,n$adj_subj)
+results <- boot(data=n, statistic=rsq, R=10000, formula=value~adj_subj)
+boot.ci(results, type="bca") 
+#r          -0.20
+#R2          0.04
+#95%   ( 0.0000,  0.3201 )
+
+###simple adj excluding accurate
+###pos
+p = aggregate(value~polarity*adjective*adj_subj*item,data=e_new[e_new$polarity=="Pos",],FUN=mean)
+gof(p$value,p$adj_subj)
+results <- boot(data=p, statistic=rsq, R=10000, formula=value~adj_subj)
+boot.ci(results, type="bca") 
+#r           0.42
+#R2          0.17
+#95%   ( 0.004,  0.459 ) 
+
+###neg
+n = aggregate(value~polarity*adjective*adj_subj*item,data=e_new[e_new$polarity=="Neg",],FUN=mean)
+gof(n$value,n$adj_subj)
+results <- boot(data=n, statistic=rsq, R=10000, formula=value~adj_subj)
+boot.ci(results, type="bca") 
+#r          -0.25
+#R2          0.06
+#95%   ( 0.0000,  0.4065 )
+
+
+
+##################################################################
+####### correlations between different subjectivity scores #######
+##################################################################
+
+### correlation between positive adjective and its negative antonym
+all_subj = aggregate(adj_subj~item*polarity,FUN=mean,data=e)
+pos_subj = all_subj[all_subj$polarity=="Pos",]
+neg_subj = all_subj[all_subj$polarity=="Neg",]
+cor.test(pos_subj$adj_subj,neg_subj$adj_subj)
+## r=0.6735
+
+
--- experiments/2-faultless-disagreement/results/analysis.R
+++ experiments/2-faultless-disagreement/results/analysis.R
@@ -1,68 +1,48 @@
 library(ggplot2)
-library(reshape2)
 library(lme4)
+library(hydroGOF)
+library(tidyr)
+library(dplyr)
 
-setwd("~/Documents/git/cocolab/adjective_ordering/experiments/2-faultless-disagreement/Submiterator-master")
-
-d = read.table("faultless-disagreement-2-trials.tsv",sep="\t",header=T)
-head(d)
-s = read.table("faultless-disagreement-2-subject_information.tsv",sep="\t",header=T)
-head(s)
-d$language = s$language[match(d$workerid,s$workerid)]
-summary(d)
-
-model.7 = lm(response~predicate, data=d)
-model.11 = lm(response~predicate+noun:predicate, data=d)
-anova(model.7,model.11)
-summary(model.7)
-summary(model.11)
-
-pairwise.t.test(d$response, d$class, p.adj = "bonf")
-
-aggregate(response~class,data=d,mean)
-
-d$class <- factor(d$class,levels=c("quality","size","age","texture","color","shape","material"))
-
-table(d$class,d$nounclass)
-
-## class plot
-d_s = bootsSummary(data=d, measurevar="response", groupvars=c("class"))
-# save data for aggregate plot
-#write.csv(d_s,"~/Documents/git/cocolab/adjective_ordering/presentations/DGfS/plots/faultless.csv")
-
-class_plot <- ggplot(d_s, aes(x=reorder(class,-response,mean),y=response)) +
-  geom_bar(stat="identity",position=position_dodge()) +
-  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(class,-response,mean), width=0.1),position=position_dodge(width=0.9))+
-  ylab("faultless disagreement\n")+
-  xlab("\nadjective class") +
-  ylim(0,1) +
-  theme_bw()
-class_plot
-ggsave("../results/class_plot.pdf",height=3)
+setwd("~/git/negative-strengthening/experiments/2-faultless-disagreement/results")
 
 
-## predicate plot by class
-p_s = bootsSummary(data=d, measurevar="response", groupvars=c("class","predicate"))
-p_s$predicate <- factor(p_s$predicate,ordered=is.ordered(p_s$predicate))
-pred_plot <- ggplot(p_s, aes(x=reorder(predicate,-response,mean),y=response)) +
-  geom_bar(stat="identity",position=position_dodge()) +
-  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(predicate,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
-  ylab("faultless disagreement\n")+
-  xlab("predicate") +
-  facet_wrap(~class,scale="free_x") +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-pred_plot
-ggsave("../results/pred_plot.pdf",height=5)
+df = read.csv("results.csv",header=T)
 
-## predicate plot by class and noun
-n_s = bootsSummary(data=d, measurevar="response", groupvars=c("class","predicate","nounclass"))
-n_s$predicate <- factor(n_s$predicate,ordered=is.ordered(n_s$predicate))
-noun_plot <- ggplot(n_s, aes(x=reorder(predicate,-response,mean),y=response,fill=nounclass)) +
-  geom_bar(stat="identity",position=position_dodge()) +
-  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(predicate,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
-  ylab("faultless disagreement\n")+
-  xlab("predicate") +
-  facet_wrap(~class,scale="free_x") +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-noun_plot
-ggsave("../results/noun_plot.pdf",height=5)
+unique(df$language)
+
+length(unique(df$participant_id)) # n=40
+
+## remove non-English speakers
+d = df[df$language!=""&
+        df$language!="GREG"
+        ,]
+unique(d$language)
+length(unique(d$participant_id)) # n=38
+
+## load helper file for bootstrapped CIs
+source("../results/helpers.r")
+
+agr = bootsSummary(data=d, measurevar="response", groupvars=c("adjective","polarity"))
+
+mean(agr$N) ## 12.78
+
+## write to CSV file
+#write.csv(agr,"adjective-faultless.csv")
+
+summary(lmer(response~polarity+(1|participant_id),data=d))
+
+
+#### compare with subjectivity
+
+sf = read.csv("~/git/negative-strengthening/experiments/1-subjectivity-negation/results/adjective-subjectivity.csv",header=T)
+
+s = sf[sf$negated=="false",]
+
+s$faultless = agr$response
+
+gof(s$response,s$faultless)
+#r         0.56
+#R2        0.32
+
+
